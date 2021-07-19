9817cf26a9f801a0ddef70ec239e8fbc34ad7192
Nelson <njjimenez21@misena.edu.co>
2021-07-19 17:33:59 -0500

    nelson

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,51 @@
+import { findLast } from '@angular/compiler/src/directive_resolver';
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  constructor() { }
+
+  private places: Place[] = [
+      {
+        id:'1',
+        title:'Inc,and Sons,LLC,Group',
+        img:'http://placeimg.com/640/480/business',
+        comments:['asdas','asdas']
+      },
+  ]
+
+  getPlaces(){
+    return [...this.places]
+  }
+  
+  getPlace(placeId:string){
+    return {
+      ...this.places.find(place => {
+        return place.id === placeId
+      })
+    }
+  }
+
+  addPlace(title:string, img:string){
+    this.places.push({
+      id:this.places.length + 1 + "",
+      title,
+      img,
+      comments:[],
+    });
+  }
+
+  deletePlace(placeId: string){
+    this.places.filter(place => {
+      return  place.id !== placeId
+    })
+  }
+
+  
+
+
+}
